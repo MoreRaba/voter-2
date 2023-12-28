3823c1171fd62d78bcc2f9700e4730e8f9fe3c6f
theScarlettCoder <egbadonjeffrey@gmail.com>
2023-12-28 07:49:42 +0000

    complete

--- app/page.js
+++ app/page.js
@@ -2,15 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 
 import Eye from "@/public/gif/wired-outline-69-eye.gif";
-import Chat from "@/public/gif/wired-lineal-981-consultation.gif";
-import Music from "@/public/gif/wired-outline-43-music-note.gif";
-import Instagram from "@/public/svg/instagram.svg";
-import Twitter from "@/public/svg/twitter.svg";
-import Facebook from "@/public/svg/facebook.svg";
+// import Chat from "@/public/gif/wired-lineal-981-consultation.gif";
+// import Music from "@/public/gif/wired-outline-43-music-note.gif";
+// import Instagram from "@/public/svg/instagram.svg";
+// import Twitter from "@/public/svg/twitter.svg";
+// import Facebook from "@/public/svg/facebook.svg";
 
 export default function Home() {
   return (
-    <main className="relative flex flex-col gap-10 items-center min-h-screen text-center my-36">
+    <main className="relative flex flex-col gap-10 items-center min-h-screen w-11/12 mx-auto text-center my-36">
       <h1 className="font-black text-4xl  lg:text-6xl text-blue-950 z-10">
         <span className="relative green-border">Start</span>,{" "}
         <span className="relative green-border">raise</span> and{" "}
@@ -19,7 +19,7 @@ export default function Home() {
       </h1>
 
       <p className="text-md lg:text-xl font-bold text-blue-950">
-        The legals for growing your company, <b>Sorted.</b>
+        The legals for the growing your voting agency, <b>Sorted.</b>
       </p>
 
       <Link
@@ -29,17 +29,17 @@ export default function Home() {
         Vote
       </Link>
 
-      <div className="hidden lg:block absolute  -top-10 left-0 -z-10">
-        <Image src={Chat} width={100} height={100} alt="Eye" />
-      </div>
+      {/* <div className="hidden lg:block absolute  -top-10 left-0 -z-10">
+        <Image src={Chat} width={100} height={100} alt="Chat" />
+      </div> */}
 
       <div className="block lg:absolute top-96 lg:top-44 right-80">
-        <Image src={Eye} width={100} height={100} alt="Chat" />
+        <Image src={Eye} width={100} height={100} alt="Eye" />
       </div>
 
-      <div className="hidden lg:absolute top-96 left-96">
+      {/*  <div className="hidden lg:absolute top-96 left-96">
         <Image src={Music} width={100} height={10} alt="Music" />
-      </div>
+      </div> */}
 
       {/* <div className="absolute -bottom-36 flex flex-row w-4/12 lg:w-2/12 justify-between items-center">
         <Image src={Instagram} width={50} height={10} alt="Instagram" />
