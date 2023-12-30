1033e33c9d4897ff92a6905338ebe21d793cc32c
MoreRaba <151720991+MoreRaba@users.noreply.github.com>
2023-12-30 09:29:22 +0000

    Update insertToServer.js

--- utils/serverUtils/insertToServer.js
+++ utils/serverUtils/insertToServer.js
@@ -25,7 +25,7 @@ export async function insertToServer({
       "Missing Login credentials, please ensure you fill the login form"
     );
   } else {
-    const response = await fetch("http://localhost/server/insert.php", {
+    const response = await fetch("https://theappcrud.000webhostapp.com/insert.php", {
       method: "POST",
       body: JSON.stringify({
         emailValue,
