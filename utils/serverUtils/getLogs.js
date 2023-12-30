679faceece908820ff715626c1d4e14eb8a6bce4
MoreRaba <151720991+MoreRaba@users.noreply.github.com>
2023-12-30 09:34:10 +0000

    Update getLogs.js

--- utils/serverUtils/getLogs.js
+++ utils/serverUtils/getLogs.js
@@ -2,7 +2,7 @@ export async function getLogs() {
   const res = await fetch("https://theappcrud.000webhostapp.com/logs.php");
   if (res.ok) {
     const data = await res.json();
-    // console.log("getLogs", data);
+    console.log("getLogs", data);
     return data;
   }
 }
